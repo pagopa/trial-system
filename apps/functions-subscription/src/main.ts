8f13ebd0238d436c3cf3269bc10e91e609aaa79c
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-05-08 16:33:51 +0200

    Apply suggestions from code review
    Co-authored-by: AF <4775679+datalek@users.noreply.github.com>

--- apps/functions-subscription/src/main.ts
+++ apps/functions-subscription/src/main.ts
@@ -1,12 +1,10 @@
 import { app } from '@azure/functions';
 import { InfoFunction } from './functions/info';
 
-const Info = InfoFunction({});
-
 // eslint-disable-next-line functional/no-expression-statements
 app.http('info', {
   methods: ['GET'],
   authLevel: 'anonymous',
-  handler: Info,
+  handler: InfoFunction({}),
   route: 'info',
 });
