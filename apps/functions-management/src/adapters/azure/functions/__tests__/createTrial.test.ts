6a1c14f4b9ff11856efdfc14851cb916e4bba893
AleDore <alessio85.dore@gmail.com>
2024-05-14 16:10:04 +0200

    fix lint

--- apps/functions-management/src/adapters/azure/functions/__tests__/createTrial.test.ts
+++ apps/functions-management/src/adapters/azure/functions/__tests__/createTrial.test.ts
@@ -1,58 +1,58 @@
-import { describe, expect, it, vi } from "vitest";
-import { makeCreateTrialHandler } from "../createTrial";
-import { httpHandlerInputMocks } from "../__mocks__/handlerMocks";
-import * as E from "fp-ts/lib/Either";
-import * as TE from "fp-ts/lib/TaskEither";
-import { TrialModel } from "../../../../models/trial";
-import * as H from "@pagopa/handler-kit";
-import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import { describe, expect, it, vi } from 'vitest';
+import { makeCreateTrialHandler } from '../createTrial';
+import { httpHandlerInputMocks } from '../__mocks__/handlerMocks';
+import * as E from 'fp-ts/lib/Either';
+import * as TE from 'fp-ts/lib/TaskEither';
+import { TrialModel } from '../../../../models/trial';
+import * as H from '@pagopa/handler-kit';
+import { NonEmptyString } from '@pagopa/ts-commons/lib/strings';
 
 const upsertMock = vi.fn();
-const trialModelMock = ({
-  upsert: upsertMock
-} as unknown) as TrialModel;
+const trialModelMock = {
+  upsert: upsertMock,
+} as unknown as TrialModel;
 
 const aValidBodyPayload = {
-  name: "TrialName" as NonEmptyString
+  name: 'TrialName' as NonEmptyString,
 };
 
 const upsertedItem = {
   ...aValidBodyPayload,
   isEnabled: true,
-  id: "TrialID" as NonEmptyString
+  id: 'TrialID' as NonEmptyString,
 };
 
-describe("Create Trial handler", () => {
-  it("should return bad request if request body is malformed", async () => {
+describe('Create Trial handler', () => {
+  it('should return bad request if request body is malformed', async () => {
     const mockReq: H.HttpRequest = {
-      ...H.request("https://api.test.it/"),
-      body: { foo: "abc" }
+      ...H.request('https://api.test.it/'),
+      body: { foo: 'abc' },
     };
     const result = await makeCreateTrialHandler({
       ...httpHandlerInputMocks,
       input: mockReq,
-      trialModel: trialModelMock
+      trialModel: trialModelMock,
     })();
     expect(upsertMock).not.toHaveBeenCalled();
     expect(E.isRight(result)).toEqual(true);
     expect(result).toMatchObject({
       right: {
         statusCode: 400,
-        body: { status: 400, title: "Missing or invalid body" }
-      }
+        body: { status: 400, title: 'Missing or invalid body' },
+      },
     });
   });
 
-  it("should return Internal Server error if upsert fails", async () => {
+  it('should return Internal Server error if upsert fails', async () => {
     const mockReq: H.HttpRequest = {
-      ...H.request("https://api.test.it/"),
-      body: aValidBodyPayload
+      ...H.request('https://api.test.it/'),
+      body: aValidBodyPayload,
     };
-    upsertMock.mockImplementationOnce(() => TE.left("Cannot upsert item"));
+    upsertMock.mockImplementationOnce(() => TE.left('Cannot upsert item'));
     const result = await makeCreateTrialHandler({
       ...httpHandlerInputMocks,
       input: mockReq,
-      trialModel: trialModelMock
+      trialModel: trialModelMock,
     })();
     expect(upsertMock).toHaveBeenCalled();
     expect(E.isRight(result)).toEqual(true);
@@ -61,25 +61,25 @@ describe("Create Trial handler", () => {
         statusCode: 500,
         body: {
           status: 500,
-          title: "An error occurred saving Trial: [Cannot upsert item]"
-        }
-      }
+          title: 'An error occurred saving Trial: [Cannot upsert item]',
+        },
+      },
     });
   });
 
-  it("should return 200 Success if upsert works", async () => {
+  it('should return 200 Success if upsert works', async () => {
     const mockReq: H.HttpRequest = {
-      ...H.request("https://api.test.it/"),
-      body: aValidBodyPayload
+      ...H.request('https://api.test.it/'),
+      body: aValidBodyPayload,
     };
     upsertMock.mockImplementationOnce(() => TE.right(upsertedItem));
     const result = await makeCreateTrialHandler({
       ...httpHandlerInputMocks,
       input: mockReq,
-      trialModel: trialModelMock
+      trialModel: trialModelMock,
     })();
     expect(upsertMock).toHaveBeenCalledWith(
-      expect.objectContaining({ ...aValidBodyPayload })
+      expect.objectContaining({ ...aValidBodyPayload }),
     );
     expect(E.isRight(result)).toEqual(true);
     expect(result).toMatchObject({
@@ -87,9 +87,9 @@ describe("Create Trial handler", () => {
         statusCode: 200,
         body: {
           trialId: upsertedItem.id,
-          isEnabled: upsertedItem.isEnabled
-        }
-      }
+          isEnabled: upsertedItem.isEnabled,
+        },
+      },
     });
   });
 });
--- apps/functions-management/src/middlewares/__tests__/request.test.ts
+++ apps/functions-management/src/middlewares/__tests__/request.test.ts
@@ -1,28 +1,27 @@
-import { describe, expect, it, vi } from 'vitest';
-import { RequiredBodyMiddleware } from "../request";
-import * as t from "io-ts";
-import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
-import * as H from "@pagopa/handler-kit";
-import { pipe } from "fp-ts/function";
-import * as TE from "fp-ts/lib/TaskEither";
-
+import { describe, expect, it } from 'vitest';
+import { RequiredBodyMiddleware } from '../request';
+import * as t from 'io-ts';
+import { NonEmptyString } from '@pagopa/ts-commons/lib/strings';
+import * as H from '@pagopa/handler-kit';
+import { pipe } from 'fp-ts/function';
+import * as TE from 'fp-ts/lib/TaskEither';
 
 const sampleSchema = t.type({
   fiscalcode: NonEmptyString,
-  sample: NonEmptyString
+  sample: NonEmptyString,
 });
-const aValidPayload = { fiscalcode: "aFiscalCode", sample: "abc" };
-describe("RequiredBodyMiddleware", () => {
-  it("should decode successfully", async () => {
+const aValidPayload = { fiscalcode: 'aFiscalCode', sample: 'abc' };
+describe('RequiredBodyMiddleware', () => {
+  it('should decode successfully', async () => {
     const mockReq: H.HttpRequest = {
-      ...H.request("https://api.test.it/"),
-      body: aValidPayload
+      ...H.request('https://api.test.it/'),
+      body: aValidPayload,
     };
 
     const result = await pipe(
       mockReq,
       RequiredBodyMiddleware(sampleSchema),
-      TE.toUnion
+      TE.toUnion,
     )();
 
     expect(result).toStrictEqual(aValidPayload);
@@ -31,19 +30,19 @@ describe("RequiredBodyMiddleware", () => {
   it.each`
     body
     ${{}}
-    ${{ fiscalcode: "", sample: "" }}
-    ${{ fiscalcode: aValidPayload.fiscalcode, sample: "" }}
-    ${{ fiscalcode: "", sample: aValidPayload.sample }}
-  `("should fail while decoding and return an HTTP error", async ({ body }) => {
+    ${{ fiscalcode: '', sample: '' }}
+    ${{ fiscalcode: aValidPayload.fiscalcode, sample: '' }}
+    ${{ fiscalcode: '', sample: aValidPayload.sample }}
+  `('should fail while decoding and return an HTTP error', async ({ body }) => {
     const mockReq: H.HttpRequest = {
-      ...H.request("https://api.test.it/"),
-      body
+      ...H.request('https://api.test.it/'),
+      body,
     };
 
     const result = await pipe(
       mockReq,
       RequiredBodyMiddleware(sampleSchema),
-      TE.toUnion
+      TE.toUnion,
     )();
 
     expect(result).toBeInstanceOf(Error);
