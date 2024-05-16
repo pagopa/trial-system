4b029ce756341e489745dbacaf2b526ddca1bd13
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-05-16 10:14:24 +0200

    Add POST to create a subscription

--- apps/functions-subscription/src/main.ts
+++ apps/functions-subscription/src/main.ts
@@ -1,5 +1,12 @@
 import { app } from '@azure/functions';
 import { makeInfoHandler } from './adapters/azure/functions/info';
+import { makePostSubscriptionHandler } from './adapters/azure/functions/subscriptions';
+import { Capabilities } from './domain/capabilities';
+
+/** FIXME: At the moment, we do not have implementations for the capabilities.
+ * To let the code properly work, we are creating an empty object.
+ */
+const capabilities = {} as unknown as Capabilities;
 
 // eslint-disable-next-line functional/no-expression-statements
 app.http('info', {
@@ -7,4 +14,12 @@ app.http('info', {
   authLevel: 'anonymous',
   handler: makeInfoHandler({}),
   route: 'info',
+});
+
+// eslint-disable-next-line functional/no-expression-statements
+app.http('/trials/{trialId}/subscriptions', {
+  methods: ['POST'],
+  authLevel: 'anonymous',
+  handler: makePostSubscriptionHandler(capabilities),
+  route: '/trials/{trialId}/subscriptions',
 });
