d8e33ff04989a6b02c5c7597c96529dc7051efda
AF <alessandro.ferlin@pagopa.it>
2024-05-16 10:35:54 +0200

    Refactor insertSubscription use-case

--- apps/functions-subscription/src/use-cases/insert-subscription.ts
+++ apps/functions-subscription/src/use-cases/insert-subscription.ts
@@ -3,7 +3,12 @@ import * as O from 'fp-ts/Option';
 import * as TE from 'fp-ts/TaskEither';
 import * as RTE from 'fp-ts/ReaderTaskEither';
 import { Capabilities } from '../domain/capabilities';
-import { TrialId, UserId, makeSubscriptionId } from '../domain/subscription';
+import {
+  SubscriptionId,
+  TrialId,
+  UserId,
+  makeSubscriptionId,
+} from '../domain/subscription';
 import { SubscriptionAlreadyExists } from './errors';
 
 // Maps all the requirements for this use-case
@@ -15,34 +20,32 @@ type Env = Pick<
   | 'hashFn'
 >;
 
+const handleSubscriptionAlreadyExists =
+  (id: SubscriptionId) => (env: Pick<Capabilities, 'subscriptionReader'>) =>
+    pipe(
+      env.subscriptionReader.get(id),
+      TE.flatMap(
+        O.fold(
+          () => TE.right(id),
+          () =>
+            TE.left(
+              new SubscriptionAlreadyExists('Subscription already exists'),
+            ),
+        ),
+      ),
+    );
+
+const handleMissingSubscription =
+  (userId: UserId, trialId: TrialId, id: SubscriptionId) => (env: Env) =>
+    pipe(
+      env.subscriptionRequestWriter.insert({ userId, trialId }),
+      TE.flatMap(() => env.subscriptionWriter.insert({ userId, trialId, id })),
+    );
+
 export const insertSubscription = (userId: UserId, trialId: TrialId) =>
   pipe(
     RTE.ask<Env>(),
     RTE.apSW('id', makeSubscriptionId(trialId, userId)),
-    RTE.flatMapTaskEither(
-      ({
-        subscriptionReader,
-        subscriptionWriter,
-        subscriptionRequestWriter,
-        id,
-      }) =>
-        pipe(
-          subscriptionReader.get(id),
-          TE.flatMap(
-            O.fold(
-              () =>
-                pipe(
-                  subscriptionRequestWriter.insert({ userId, trialId }),
-                  TE.flatMap(() =>
-                    subscriptionWriter.insert({ userId, trialId, id }),
-                  ),
-                ),
-              () =>
-                TE.left(
-                  new SubscriptionAlreadyExists('Subscription already exists'),
-                ),
-            ),
-          ),
-        ),
-    ),
+    RTE.chainFirstW(({ id }) => handleSubscriptionAlreadyExists(id)),
+    RTE.chainW(({ id }) => handleMissingSubscription(userId, trialId, id)),
   );
