eb55bab11b332c6df1804470b2c7892dd6b4ff8b
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-05-17 17:30:56 +0200

    Rename type

--- apps/functions-subscription/src/system-env.ts
+++ apps/functions-subscription/src/system-env.ts
@@ -2,7 +2,7 @@ import * as RTE from 'fp-ts/ReaderTaskEither';
 import * as TE from 'fp-ts/TaskEither';
 import { insertSubscription } from './use-cases/insert-subscription';
 
-type Test<T> = T extends (
+type RemoveReaderType<T> = T extends (
   ...args: infer I
   /*eslint-disable-next-line @typescript-eslint/no-explicit-any */
 ) => RTE.ReaderTaskEither<any, infer E, infer A>
@@ -10,5 +10,5 @@ type Test<T> = T extends (
   : never;
 
 export interface SystemEnv {
-  readonly insertSubscription: Test<typeof insertSubscription>;
+  readonly insertSubscription: RemoveReaderType<typeof insertSubscription>;
 }
