ce909efe0802e0557dfc4ee1b07acc21d16af249
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-05-20 17:12:32 +0200

    Create event hub

---
+++ infra/resources/modules/commons/eventhub.tf
@@ -0,0 +1,23 @@
+module "subscription_requests_event_hub" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//eventhub?ref=v8.13.0"
+
+  name                = "${local.domain}-evh-${local.application_basename}"
+  resource_group_name = azurerm_resource_group.data_rg.name
+  location            = var.location
+  sku                 = "Standard"
+
+  eventhubs = [
+    // EventHub that collects users' subscription requests
+    {
+      name              = "${local.domain}-subscription-requests"
+      partitions        = 1
+      message_retention = 90 # 90 days, set to the max value
+      consumers         = []
+      keys              = []
+    },
+  ]
+
+  private_endpoint_created = false
+  virtual_network_ids      = [azurerm_virtual_network.vnet.id]
+  tags                     = var.tags
+}
