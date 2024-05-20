a1c71f44d425d8ad2f597f0e7986d16587bee381
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-05-20 17:55:55 +0200

    Add comment

--- apps/functions-subscription/src/system-env.ts
+++ apps/functions-subscription/src/system-env.ts
@@ -2,6 +2,10 @@ import * as RTE from 'fp-ts/ReaderTaskEither';
 import * as TE from 'fp-ts/TaskEither';
 import { insertSubscription } from './use-cases/insert-subscription';
 
+/*
+ * This is an utility to extract the TaskEither type from a
+ * ReaderTaskEither.
+ */
 type RemoveReaderType<T> = T extends (
   ...args: infer I
   /*eslint-disable-next-line @typescript-eslint/no-explicit-any */
