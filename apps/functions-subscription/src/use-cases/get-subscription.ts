23cee0daba8e07100b08c76015e37877586f6d7b
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-05-21 14:44:28 +0200

    Update implementation of get subscription use case

--- apps/functions-subscription/src/use-cases/get-subscription.ts
+++ apps/functions-subscription/src/use-cases/get-subscription.ts
@@ -1,14 +1,34 @@
 import { pipe } from 'fp-ts/lib/function';
 import * as RTE from 'fp-ts/ReaderTaskEither';
 import { Capabilities } from '../domain/capabilities';
-import { TrialId, UserId, makeSubscriptionId } from '../domain/subscription';
+import {
+  TrialId,
+  UserId,
+  makeSubscriptionId,
+  SubscriptionId,
+} from '../domain/subscription';
+import * as TE from 'fp-ts/TaskEither';
+import * as O from 'fp-ts/Option';
+import { ItemNotFound } from '../domain/errors';
 
 // Maps all the requirements for this use-case
 type Env = Pick<Capabilities, 'subscriptionReader' | 'hashFn'>;
+
+const getSubscriptionById =
+  (id: SubscriptionId) => (env: Pick<Capabilities, 'subscriptionReader'>) =>
+    pipe(
+      env.subscriptionReader.get(id),
+      TE.flatMap(
+        O.fold(
+          () => TE.left(new ItemNotFound(`Subscription ${id} not found`)),
+          (subscription) => TE.right(subscription),
+        ),
+      ),
+    );
 
 export const getSubscription = (userId: UserId, trialId: TrialId) =>
   pipe(
     RTE.ask<Env>(),
     RTE.apSW('id', makeSubscriptionId(trialId, userId)),
-    RTE.map(({ id, subscriptionReader }) => pipe(subscriptionReader.get(id))),
+    RTE.chainW(({ id }) => getSubscriptionById(id)),
   );
