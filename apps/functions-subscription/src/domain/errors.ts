d0c384bf8028077036ed3640e10554f5d3849d9e
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-05-21 14:24:18 +0200

    Create ItemNotFound error

--- apps/functions-subscription/src/domain/errors.ts
+++ apps/functions-subscription/src/domain/errors.ts
@@ -5,3 +5,7 @@ export class TooManyRequestsError extends Error {
 export class ItemAlreadyExists extends Error {
   readonly _tag = 'ItemAlreadyExists';
 }
+
+export class ItemNotFound extends Error {
+  readonly _tag = 'ItemNotFound';
+}
