b060864be89fecacf05db189ab1e8a2d4d584e48
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-05-21 14:42:41 +0200

    Update unit tests for the use case

--- apps/functions-subscription/src/use-cases/__tests__/get-subscription.test.ts
+++ apps/functions-subscription/src/use-cases/__tests__/get-subscription.test.ts
@@ -26,14 +26,16 @@ describe('getSubscription', () => {
 
   it('should return ItemNotFound when the subscription does not exist', async () => {
     const testEnv = makeTestEnv();
-    const error = new ItemNotFound('Item not found');
+    const notFoundError = new ItemNotFound(
+      `Subscription ${aSubscription.id} not found`,
+    );
 
     testEnv.hashFn.mockReturnValueOnce({ value: aSubscription.id });
     testEnv.subscriptionReader.get.mockReturnValueOnce(TE.right(O.none));
 
     const actual = await getSubscription(userId, trialId)(testEnv)();
-    const expected = E.left(error);
-    expect(actual).toMatchObject(expected);
+    const expected = E.left(notFoundError);
+    expect(actual).toStrictEqual(expected);
   });
 
   it('should return error if something fail', async () => {
