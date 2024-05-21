be38cb1c3f5932952c8c0b21fde489ea9b244b79
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-05-21 14:23:35 +0200

    Update system env with new use case

--- apps/functions-subscription/src/system-env.ts
+++ apps/functions-subscription/src/system-env.ts
@@ -1,9 +1,11 @@
 import { flow, apply } from 'fp-ts/lib/function';
 import { insertSubscription } from './use-cases/insert-subscription';
+import { getSubscription } from './use-cases/get-subscription';
 import { Capabilities } from './domain/capabilities';
 
 export const makeSystemEnv = (capabilities: Capabilities) => ({
   insertSubscription: flow(insertSubscription, apply(capabilities)),
+  getSubscription: flow(getSubscription, apply(capabilities)),
 });
 
 export type SystemEnv = ReturnType<typeof makeSystemEnv>;
