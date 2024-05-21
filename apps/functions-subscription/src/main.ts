a4e61901adcfd24e4e2db25df7787390e7d9504d
AF <alessandro.ferlin@pagopa.it>
2024-05-21 09:34:51 +0200

    Refactor main

--- apps/functions-subscription/src/main.ts
+++ apps/functions-subscription/src/main.ts
@@ -1,64 +1,62 @@
 import { pipe } from 'fp-ts/lib/function';
-import * as TE from 'fp-ts/lib/TaskEither';
+import * as E from 'fp-ts/lib/Either';
 import { app } from '@azure/functions';
-import { parseConfig } from './config';
 import { EventHubProducerClient } from '@azure/event-hubs';
 import { CosmosClient } from '@azure/cosmos';
 import { makeInfoHandler } from './adapters/azure/functions/info';
 import { makePostSubscriptionHandler } from './adapters/azure/functions/subscriptions';
 import { makeSubscriptionCosmosContainer } from './adapters/azure/cosmosdb/subscription';
 import { makeSubscriptionRequestEventHubProducer } from './adapters/azure/eventhubs/subscription-request';
-import { SystemEnv } from './system-env';
+import { parseConfig } from './config';
+import { Capabilities } from './domain/capabilities';
+import { makeSystemEnv } from './system-env';
+import { clock } from './adapters/date/clock';
+import { hashFn } from './adapters/crypto/hash';
 
-/**
- * FIXME: At the moment, we do not have implementations for the env.
- * To let the code properly work, we are creating an empty object.
- */
-const env = {} as unknown as SystemEnv;
-
-// the application entry-point
-pipe(
-  TE.Do,
-  TE.apS('config', TE.fromEither(parseConfig(process.env))),
-  TE.bind('cosmosDB', ({ config }) =>
-    TE.of(new CosmosClient(config.subscription.cosmosdb.connectionString)),
-  ),
-  TE.bind('subscriptionRequestEventHub', ({ config }) =>
-    TE.of(
-      new EventHubProducerClient(
-        config.subscriptionRequest.eventhub.connectionString,
-        config.subscriptionRequest.eventhub.name,
-      ),
-    ),
-  ),
-
-  TE.bind('subscriptionReaderWriter', ({ config, cosmosDB }) =>
-    TE.of(
-      makeSubscriptionCosmosContainer(
-        cosmosDB.database(config.subscription.cosmosdb.databaseName),
-      ),
-    ),
-  ),
-  TE.bind('subscriptionRequestWriter', ({ subscriptionRequestEventHub }) =>
-    TE.of(makeSubscriptionRequestEventHubProducer(subscriptionRequestEventHub)),
-  ),
-  // eslint-disable-next-line functional/no-return-void
-  TE.map(({ cosmosDB, subscriptionRequestEventHub }) => {
-    app.http('info', {
-      methods: ['GET'],
-      authLevel: 'anonymous',
-      handler: makeInfoHandler({ cosmosDB, subscriptionRequestEventHub }),
-      route: 'info',
-    });
-    app.http('createSubscription', {
-      methods: ['POST'],
-      authLevel: 'anonymous',
-      handler: makePostSubscriptionHandler(env),
-      route: '/trials/{trialId}/subscriptions',
-    });
-  }),
-  TE.getOrElse((error) => {
+const config = pipe(
+  parseConfig(process.env),
+  E.getOrElseW((error) => {
     // eslint-disable-next-line functional/no-throw-statements
     throw new Error(error);
   }),
-)();
+);
+
+const cosmosDB = new CosmosClient(
+  config.subscription.cosmosdb.connectionString,
+);
+
+const subscriptionRequestEventHub = new EventHubProducerClient(
+  config.subscriptionRequest.eventhub.connectionString,
+  config.subscriptionRequest.eventhub.name,
+);
+
+const subscriptionReaderWriter = makeSubscriptionCosmosContainer(
+  cosmosDB.database(config.subscription.cosmosdb.databaseName),
+);
+
+const subscriptionRequestWriter = makeSubscriptionRequestEventHubProducer(
+  subscriptionRequestEventHub,
+);
+
+const capabilities: Capabilities = {
+  subscriptionReader: subscriptionReaderWriter,
+  subscriptionWriter: subscriptionReaderWriter,
+  subscriptionRequestWriter,
+  hashFn,
+  clock,
+};
+
+const env = makeSystemEnv(capabilities);
+
+app.http('info', {
+  methods: ['GET'],
+  authLevel: 'anonymous',
+  handler: makeInfoHandler({ cosmosDB, subscriptionRequestEventHub }),
+  route: 'info',
+});
+app.http('createSubscription', {
+  methods: ['POST'],
+  authLevel: 'anonymous',
+  handler: makePostSubscriptionHandler(env),
+  route: '/trials/{trialId}/subscriptions',
+});
