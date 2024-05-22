a8b279409f724276b0ac83a1fd519559a24c001a
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-05-22 11:05:35 +0200

    Remove internal function

--- apps/functions-subscription/src/use-cases/get-subscription.ts
+++ apps/functions-subscription/src/use-cases/get-subscription.ts
@@ -1,30 +1,23 @@
 import { pipe } from 'fp-ts/lib/function';
 import * as RTE from 'fp-ts/ReaderTaskEither';
 import { Capabilities } from '../domain/capabilities';
-import {
-  TrialId,
-  UserId,
-  makeSubscriptionId,
-  SubscriptionId,
-} from '../domain/subscription';
+import { TrialId, UserId, makeSubscriptionId } from '../domain/subscription';
 import * as TE from 'fp-ts/TaskEither';
 import { ItemNotFound } from '../domain/errors';
 
 // Maps all the requirements for this use-case
 type GetSubscriptionEnv = Pick<Capabilities, 'subscriptionReader'>;
 
-const getSubscriptionById =
-  (id: SubscriptionId) => (env: Pick<Capabilities, 'subscriptionReader'>) =>
-    pipe(
-      env.subscriptionReader.get(id),
-      TE.flatMap(
-        TE.fromOption(() => new ItemNotFound(`Subscription ${id} not found`)),
-      ),
-    );
-
 export const getSubscription = (userId: UserId, trialId: TrialId) =>
   pipe(
     RTE.ask<GetSubscriptionEnv>(),
     RTE.apSW('id', makeSubscriptionId(trialId, userId)),
-    RTE.flatMap(({ id }) => getSubscriptionById(id)),
+    RTE.flatMapTaskEither(({ subscriptionReader, id }) =>
+      pipe(
+        subscriptionReader.get(id),
+        TE.flatMap(
+          TE.fromOption(() => new ItemNotFound(`Subscription ${id} not found`)),
+        ),
+      ),
+    ),
   );
