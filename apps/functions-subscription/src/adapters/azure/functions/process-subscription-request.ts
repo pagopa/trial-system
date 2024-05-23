926f7ec592c44f02809bd81234f8efa860a0ba30
AF <alessandro.ferlin@pagopa.it>
2024-05-23 09:16:51 +0200

    Remove return

--- apps/functions-subscription/src/adapters/azure/functions/process-subscription-request.ts
+++ apps/functions-subscription/src/adapters/azure/functions/process-subscription-request.ts
@@ -14,15 +14,14 @@ export const makeSubscriptionRequestConsumerHandler =
     messages: unknown,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     _: InvocationContext,
-  ): Promise<readonly SubscriptionRequest[]> => {
-    return pipe(
+  ): Promise<readonly SubscriptionRequest[]> =>
+    pipe(
       TE.fromEither(t.array(SubscriptionRequestCodec).decode(messages)),
       TE.chainW(TE.traverseArray(env.processSubscriptionRequest)),
       TE.getOrElse((error) => {
         // eslint-disable-next-line functional/no-promise-reject
         return () => Promise.reject(error);
       }),
-      // run the task returning a primise
+      // run the task returning a promise
       (task) => task(),
     );
-  };
