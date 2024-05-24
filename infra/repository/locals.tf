4bacd9c00a735314dbb183ea83263654d46e8c31
Andrea Grillo <andrea.grillo@pagopa.it>
2024-05-24 11:05:46 +0200

    add env app and reviewers

--- infra/repository/locals.tf
+++ infra/repository/locals.tf
@@ -21,6 +21,6 @@ locals {
     secrets = {
       "ARM_CLIENT_ID" = data.azurerm_user_assigned_identity.identity_prod_cd.client_id
     }
+    reviewers_teams = ["io-platform-contributors"]
   }
-
 }
