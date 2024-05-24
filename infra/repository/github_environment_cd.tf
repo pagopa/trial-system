ab8d5ebb9a8af49b74999bf2c6c1c2b6071dbc5a
Andrea Grillo <andrea.grillo@pagopa.it>
2024-05-24 11:11:25 +0200

    [EC-309] Add GitHub environment for App deployment and reviewers for infra deployment (#45)

--- infra/repository/github_environment_cd.tf
+++ infra/repository/github_environment_cd.tf
@@ -6,6 +6,24 @@ resource "github_repository_environment" "github_repository_environment_prod_cd"
     protected_branches     = false
     custom_branch_policies = true
   }
+
+  reviewers {
+    teams = matchkeys(
+      data.github_organization_teams.all.teams[*].id,
+      data.github_organization_teams.all.teams[*].slug,
+      local.cd.reviewers_teams
+    )
+  }
+}
+
+resource "github_repository_environment" "github_repository_environment_app_prod_cd" {
+  environment = "app-prod-cd"
+  repository  = github_repository.this.name
+
+  deployment_branch_policy {
+    protected_branches     = false
+    custom_branch_policies = true
+  }
 }
 
 resource "github_actions_environment_secret" "env_prod_cd_secrets" {
@@ -13,6 +31,15 @@ resource "github_actions_environment_secret" "env_prod_cd_secrets" {
 
   repository      = github_repository.this.name
   environment     = github_repository_environment.github_repository_environment_prod_cd.environment
+  secret_name     = each.key
+  plaintext_value = each.value
+}
+
+resource "github_actions_environment_secret" "env_app_prod_cd_secrets" {
+  for_each = local.cd.secrets
+
+  repository      = github_repository.this.name
+  environment     = github_repository_environment.github_repository_environment_app_prod_cd.environment
   secret_name     = each.key
   plaintext_value = each.value
 }
