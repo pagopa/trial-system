8ea7a288d3138d632c602adfdb42357d8d4401b1
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-06-04 11:49:01 +0200

    Add activationService capability

--- apps/functions-subscription/src/domain/capabilities.ts
+++ apps/functions-subscription/src/domain/capabilities.ts
@@ -5,6 +5,7 @@ import { Clock } from './clock';
 import { HashFn } from './hash';
 import { SubscriptionReader, SubscriptionWriter } from './subscription';
 import { SubscriptionRequestWriter } from './subscription-request';
+import { ActivationService } from './activation';
 
 /**
  * Maps the capabilities to a given property name. Pick the capability using the
@@ -16,6 +17,7 @@ export interface Capabilities {
   readonly subscriptionReader: SubscriptionReader;
   readonly subscriptionWriter: SubscriptionWriter;
   readonly subscriptionRequestWriter: SubscriptionRequestWriter;
+  readonly activationService: ActivationService;
   readonly hashFn: HashFn;
   readonly clock: Clock;
 }
