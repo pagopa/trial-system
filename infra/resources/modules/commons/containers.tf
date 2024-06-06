78464c72faceaddcb11de8ec9c7240aedbb3213e
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-06-06 11:58:46 +0200

    [IOPLT-545] Create Cosmos DB activations container (#51)

--- infra/resources/modules/commons/containers.tf
+++ infra/resources/modules/commons/containers.tf
@@ -46,6 +46,51 @@ resource "azurerm_cosmosdb_sql_container" "subscription_history" {
   }
 }
 
+resource "azurerm_cosmosdb_sql_container" "activations" {
+  name                  = "activations"
+  resource_group_name   = azurerm_resource_group.data_rg.name
+  account_name          = module.cosmosdb_account.name
+  database_name         = module.cosmosdb_sql_database_trial.name
+  partition_key_path    = "/trialId"
+  partition_key_version = 2
+
+  autoscale_settings {
+    max_throughput = 1000
+  }
+
+  indexing_policy {
+    included_path {
+      path = "/*"
+    }
+
+    excluded_path {
+      path = "/\"_etag\"/?"
+    }
+
+    composite_index {
+      index {
+        path  = "/trialId"
+        order = "Ascending"
+      }
+
+      index {
+        path  = "/type"
+        order = "Ascending"
+      }
+
+      index {
+        path  = "/activated"
+        order = "Ascending"
+      }
+
+      index {
+        path  = "/id"
+        order = "Ascending"
+      }
+    }
+  }
+}
+
 resource "azurerm_cosmosdb_sql_container" "leases" {
   name                  = "leases"
   resource_group_name   = azurerm_resource_group.data_rg.name
