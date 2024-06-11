0d3c3e53df31fb09f495c1d33e4d2ee452f90060
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-06-11 11:28:35 +0200

    Add handler signature

--- apps/functions-subscription/src/adapters/azure/functions/create-activation-job.ts
+++ apps/functions-subscription/src/adapters/azure/functions/create-activation-job.ts
@@ -2,8 +2,13 @@ import * as H from '@pagopa/handler-kit';
 import { httpAzureFunction } from '@pagopa/handler-kit-azure-func';
 import { flow, pipe } from 'fp-ts/lib/function';
 import * as RTE from 'fp-ts/lib/ReaderTaskEither';
+import { SystemEnv } from '../../../system-env';
 
-const makeHandlerKitHandler = H.of(() =>
+const makeHandlerKitHandler: H.Handler<
+  H.HttpRequest,
+  H.HttpResponse<string> | H.HttpResponse<H.ProblemJson, H.HttpErrorStatusCode>,
+  SystemEnv
+> = H.of(() =>
   pipe(
     // FIXME: Fix implementation
     RTE.of('Hello world!'),
