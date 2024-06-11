431497bd04f11f5692fbb4782e95150b697c7e1d
AF <alessandro.ferlin@pagopa.it>
2024-06-11 12:09:25 +0200

    Add environment to azure functions

--- infra/resources/modules/commons/function_subscription.tf
+++ infra/resources/modules/commons/function_subscription.tf
@@ -16,11 +16,17 @@ locals {
     FETCH_KEEPALIVE_FREE_SOCKET_TIMEOUT = "30000"
     FETCH_KEEPALIVE_TIMEOUT             = "60000"
 
-    COSMOSDB_ENDPOINT                  = module.cosmosdb_account.endpoint
-    COSMOSDB_DATABASE_NAME             = module.cosmosdb_sql_database_trial.name
-    EVENTHUB_NAMESPACE                 = "${module.event_hub.name}.servicebus.windows.net"
-    SUBSCRIPTION_REQUEST_EVENTHUB_NAME = "${local.domain}-subscription-requests"
+    COSMOSDB_ENDPOINT      = module.cosmosdb_account.endpoint
+    COSMOSDB_DATABASE_NAME = module.cosmosdb_sql_database_trial.name
+    EVENTHUB_NAMESPACE     = "${module.event_hub.name}.servicebus.windows.net"
+
+    LEASES_COSMOSDB_CONTAINER_NAME = azurerm_cosmosdb_sql_container.leases.name
+
+    SUBSCRIPTION_HISTORY_CONSUMER                = "off"
+    SUBSCRIPTION_HISTORY_COSMOSDB_CONTAINER_NAME = azurerm_cosmosdb_sql_container.subscription_history.name
+
     SUBSCRIPTION_REQUEST_CONSUMER      = "off"
+    SUBSCRIPTION_REQUEST_EVENTHUB_NAME = "${local.domain}-subscription-requests"
   }
 }
 
