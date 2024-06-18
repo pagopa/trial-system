f72e36a60cedbea2f27fb367fab9f8a3b83af7bd
AF <alessandro.ferlin@pagopa.it>
2024-06-18 10:02:23 +0200

    Refactor domain mocks

--- apps/functions-subscription/src/domain/__tests__/mocks.ts
+++ apps/functions-subscription/src/domain/__tests__/mocks.ts
@@ -1,29 +1,17 @@
 import { mock, mockFn } from 'vitest-mock-extended';
-import { SubscriptionReader, SubscriptionWriter } from '../subscription';
-import { SubscriptionRequestWriter } from '../subscription-request';
-import {
-  SubscriptionHistoryReader,
-  SubscriptionHistoryWriter,
-} from '../subscription-history';
-import { EventWriter } from '../event';
-import { ActivationJobWriter } from '../activation-job';
-import { ActivationRequestRepository } from '../activation-request';
-import { Clock } from '../clock';
-import { MonotonicIdFn } from '../monotonic-id';
-import { HashFn } from '../hash';
+import { Capabilities } from '../capabilities';
 
-export const makeTestEnv = () => {
-  return {
-    subscriptionReader: mock<SubscriptionReader>(),
-    subscriptionWriter: mock<SubscriptionWriter>(),
-    subscriptionRequestWriter: mock<SubscriptionRequestWriter>(),
-    subscriptionHistoryReader: mock<SubscriptionHistoryReader>(),
-    subscriptionHistoryWriter: mock<SubscriptionHistoryWriter>(),
-    activationJobWriter: mock<ActivationJobWriter>(),
-    activationRequestRepository: mock<ActivationRequestRepository>(),
-    eventWriter: mock<EventWriter>(),
-    clock: mock<Clock>(),
-    hashFn: mockFn<HashFn>(),
-    monotonicIdFn: mockFn<MonotonicIdFn>(),
-  };
-};
+export const makeTestEnv = () => ({
+  subscriptionReader: mock<Capabilities['subscriptionReader']>(),
+  subscriptionWriter: mock<Capabilities['subscriptionWriter']>(),
+  subscriptionRequestWriter: mock<Capabilities['subscriptionRequestWriter']>(),
+  subscriptionHistoryReader: mock<Capabilities['subscriptionHistoryReader']>(),
+  subscriptionHistoryWriter: mock<Capabilities['subscriptionHistoryWriter']>(),
+  activationJobWriter: mock<Capabilities['activationJobWriter']>(),
+  activationRequestRepository:
+    mock<Capabilities['activationRequestRepository']>(),
+  eventWriter: mock<Capabilities['eventWriter']>(),
+  clock: mock<Capabilities['clock']>(),
+  hashFn: mockFn<Capabilities['hashFn']>(),
+  monotonicIdFn: mockFn<Capabilities['monotonicIdFn']>(),
+});
