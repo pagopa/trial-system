24d1611dbfca835ddd88e3bb4e011d8df5bfeecf
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-06-18 10:52:37 +0200

    Refactor on get subscription use case

--- apps/functions-subscription/src/use-cases/get-subscription.ts
+++ apps/functions-subscription/src/use-cases/get-subscription.ts
@@ -1,6 +1,5 @@
 import { pipe } from 'fp-ts/lib/function';
 import * as RTE from 'fp-ts/ReaderTaskEither';
-import { Capabilities } from '../domain/capabilities';
 import {
   TrialId,
   UserId,
@@ -9,19 +8,12 @@ import {
 } from '../domain/subscription';
 import { ItemNotFound } from '../domain/errors';
 
-// Maps all the requirements for this use-case
-type GetSubscriptionEnv = Pick<Capabilities, 'subscriptionReader'>;
-
 export const getSubscription = (userId: UserId, trialId: TrialId) =>
   pipe(
-    RTE.ask<GetSubscriptionEnv>(),
-    RTE.apSW('id', makeSubscriptionId(trialId, userId)),
-    RTE.flatMap(({ id }) =>
-      pipe(
-        getSubscriptionById(id),
-        RTE.flatMap(
-          RTE.fromOption(() => new ItemNotFound('Subscription not found')),
-        ),
-      ),
+    makeSubscriptionId(trialId, userId),
+    RTE.flatMap(getSubscriptionById),
+    RTE.flatMapOption(
+      (subscription) => subscription,
+      () => new ItemNotFound('Subscription not found'),
     ),
   );
