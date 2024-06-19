b88674cae41da72bc9648c4e06de1d26f59aa20b
AF <alessandro.ferlin@pagopa.it>
2024-06-19 18:05:33 +0200

    Remove duplicate imports

--- apps/functions-subscription/src/adapters/azure/functions/codec.ts
+++ apps/functions-subscription/src/adapters/azure/functions/codec.ts
@@ -2,9 +2,7 @@ import { ActivationJob } from '../../../domain/activation-job';
 import { Subscription } from '../../../domain/subscription';
 import { ActivationJob as ActivationJobAPI } from '../../../generated/definitions/internal/ActivationJob';
 import { Subscription as SubscriptionAPI } from '../../../generated/definitions/internal/Subscription';
-import { ActivationJob as ActivationJobAPI } from '../../../generated/definitions/internal/ActivationJob';
 import { SubscriptionStateEnum } from '../../../generated/definitions/internal/SubscriptionState';
-import { ActivationJob } from '../../../domain/activation-job';
 
 export const toSubscriptionAPI = (
   subscription: Subscription,
