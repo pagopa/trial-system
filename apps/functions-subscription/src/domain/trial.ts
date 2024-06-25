421da47f869120490b5663f92e55590c4e5b51b2
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-06-25 10:26:19 +0200

    Create trial into the domain

---
+++ apps/functions-subscription/src/domain/trial.ts
@@ -0,0 +1,14 @@
+import * as t from 'io-ts';
+import { NonEmptyString } from '@pagopa/ts-commons/lib/strings';
+
+// a unique brand for trialId
+interface TrialIdBrand {
+  // use `unique symbol` here to ensure uniqueness across modules / packages
+  readonly TrialId: unique symbol;
+}
+export const TrialIdCodec = t.brand(
+  NonEmptyString,
+  (str): str is t.Branded<NonEmptyString, TrialIdBrand> => str.length > 0,
+  'TrialId',
+);
+export type TrialId = t.TypeOf<typeof TrialIdCodec>;
