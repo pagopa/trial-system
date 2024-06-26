a40d938a2b892f7544f73c0e801aae58113c76be
AleDore <alessio85.dore@gmail.com>
2024-06-26 16:43:53 +0200

    fix rg

--- infra/resources/modules/commons/dns.tf
+++ infra/resources/modules/commons/dns.tf
@@ -1,14 +1,7 @@
-resource "azurerm_resource_group" "domain_rg" {
-  name     = "${local.project}-domain-rg-01"
-  location = var.location
-
-  tags = var.tags
-}
-
 resource "azurerm_dns_zone" "trial_pagopa_it" {
   count               = (var.dns_config == null) ? 0 : 1
   name                = join(".", [var.dns_config.external_third_level, var.dns_config.second_level])
-  resource_group_name = azurerm_resource_group.domain_rg.name
+  resource_group_name = azurerm_resource_group.net_rg.name
 
   tags = var.tags
 }
@@ -16,7 +9,7 @@ resource "azurerm_dns_zone" "trial_pagopa_it" {
 resource "azurerm_dns_caa_record" "trial_pagopa_it" {
   name                = "@"
   zone_name           = azurerm_dns_zone.trial_pagopa_it[0].name
-  resource_group_name = azurerm_resource_group.domain_rg.name
+  resource_group_name = azurerm_resource_group.net_rg.name
   ttl                 = var.dns_config.dns_default_ttl_sec
 
   record {
@@ -42,7 +35,7 @@ resource "azurerm_dns_caa_record" "trial_pagopa_it" {
 resource "azurerm_dns_a_record" "api_trial_pagopa_it" {
   name                = "api"
   zone_name           = azurerm_dns_zone.trial_pagopa_it[0].name
-  resource_group_name = azurerm_resource_group.domain_rg.name
+  resource_group_name = azurerm_resource_group.net_rg.name
   ttl                 = var.dns_config.dns_default_ttl_sec
   records             = [azurerm_public_ip.appgateway_public_ip.ip_address]
 
