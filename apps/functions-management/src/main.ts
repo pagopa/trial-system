42cbcb7cb28cdb0e14ece8d43f7b0d90d8089135
Alessio Dore <57567806+AleDore@users.noreply.github.com>
2024-07-10 15:58:06 +0200

    [#IOPLT-449] Add Create Trial Handler (#22)

--- apps/functions-management/src/main.ts
+++ apps/functions-management/src/main.ts
@@ -2,11 +2,16 @@ import { app } from '@azure/functions';
 import { CosmosClient } from '@azure/cosmos';
 import { InfoFunction } from './adapters/azure/functions/info';
 import { getConfigOrThrow } from './utils/config';
+import { TRIAL_COLLECTION_NAME, TrialModel } from './models/trial';
+import { CreateTrialFunction } from './adapters/azure/functions/createTrial';
 
 const config = getConfigOrThrow();
 
 const cosmosClient = new CosmosClient(config.COSMOS_CONNECTION_STRING);
 const database = cosmosClient.database(config.COSMOS_DB_NAME);
+
+const trialContainer = database.container(TRIAL_COLLECTION_NAME);
+const trialModel = new TrialModel(trialContainer);
 
 // eslint-disable-next-line functional/no-expression-statements
 app.http('info', {
@@ -14,4 +19,12 @@ app.http('info', {
   authLevel: 'anonymous',
   handler: InfoFunction({ db: database }),
   route: 'info',
+});
+
+// eslint-disable-next-line functional/no-expression-statements
+app.http('trials', {
+  methods: ['POST'],
+  authLevel: 'function',
+  handler: CreateTrialFunction({ trialModel }),
+  route: 'api/v1/trials',
 });
---
+++ apps/functions-management/src/utils/trials/dependency.ts
@@ -0,0 +1,5 @@
+import { TrialModel } from '../../models/trial';
+
+export interface TrialModelDependency {
+  readonly trialModel: TrialModel;
+}
