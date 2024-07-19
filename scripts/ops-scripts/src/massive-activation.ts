b6286c3baf466427f9f5d39a2f29afaad0175fde
AF <alessandro.ferlin@pagopa.it>
2024-07-19 15:25:42 +0200

    Add eslint and run lint

--- scripts/ops-scripts/src/massive-activation.ts
+++ scripts/ops-scripts/src/massive-activation.ts
@@ -1,16 +1,16 @@
-import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
-import { createClient } from "./generated/definitions/trial/client";
-import * as t from "io-ts";
-import { getConfigOrThrow } from "./utils/config";
-import { flow, pipe } from "fp-ts/lib/function";
-import * as E from "fp-ts/Either";
-import * as TE from "fp-ts/TaskEither";
-import * as RA from "fp-ts/ReadonlyArray";
-import { withDefault } from "@pagopa/ts-commons/lib/types";
-import { parse } from "csv-parse/sync";
-import * as fs from "fs";
-import { errorsToReadableMessages } from "@pagopa/ts-commons/lib/reporters";
-import { CreateSubscriptionStateEnum } from "./generated/definitions/trial/CreateSubscriptionState";
+import { NonEmptyString } from '@pagopa/ts-commons/lib/strings';
+import { createClient } from './generated/definitions/trial/client';
+import * as t from 'io-ts';
+import { getConfigOrThrow } from './utils/config';
+import { flow, pipe } from 'fp-ts/lib/function';
+import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
+import * as RA from 'fp-ts/ReadonlyArray';
+import { withDefault } from '@pagopa/ts-commons/lib/types';
+import { parse } from 'csv-parse/sync';
+import * as fs from 'fs';
+import { errorsToReadableMessages } from '@pagopa/ts-commons/lib/reporters';
+import { CreateSubscriptionStateEnum } from './generated/definitions/trial/CreateSubscriptionState';
 
 const EnvsCodec = t.type({
   TRIAL_ID: NonEmptyString,
@@ -18,46 +18,46 @@ const EnvsCodec = t.type({
   TRIAL_APIM_SUBSCRIPTION_KEY: NonEmptyString,
   USER_FILE_PATH: withDefault(
     NonEmptyString,
-    "../../.data/users.csv" as NonEmptyString
-  )
+    '../../.data/users.csv' as NonEmptyString,
+  ),
 });
 
 const UserListCodec = t.readonlyArray(t.readonlyArray(NonEmptyString));
 
 const config = getConfigOrThrow(EnvsCodec, process.env);
 
-const trialClient = createClient<"ApiKeyAuth">({
-  basePath: "",
+const trialClient = createClient<'ApiKeyAuth'>({
+  basePath: '',
   baseUrl: config.TRIAL_API_BASE_URL,
   fetchApi: fetch,
-  withDefaults: op => params =>
+  withDefaults: (op) => (params) =>
     op({
       ...params,
-      ApiKeyAuth: config.TRIAL_APIM_SUBSCRIPTION_KEY
-    })
+      ApiKeyAuth: config.TRIAL_APIM_SUBSCRIPTION_KEY,
+    }),
 });
 
 export const runMassiveActivation = pipe(
   console.log(`Read CSV users file from ${config.USER_FILE_PATH}`),
   () => fs.readFileSync(config.USER_FILE_PATH),
-  userFileBuffer =>
+  (userFileBuffer) =>
     parse(userFileBuffer, {
       skip_empty_lines: true,
       columns: false,
-      from_line: 2
+      from_line: 2,
     }),
   UserListCodec.decode,
-  E.mapLeft(errs => Error(errorsToReadableMessages(errs).join("|"))),
+  E.mapLeft((errs) => Error(errorsToReadableMessages(errs).join('|'))),
   E.chain(
     E.fromPredicate(RA.isNonEmpty, () =>
-      Error("No users to activate from user's file")
-    )
+      Error("No users to activate from user's file"),
+    ),
   ),
   E.map(RA.flatten),
   E.map((users) =>
     pipe(
       users,
-      RA.traverse(TE.ApplicativeSeq)(userId =>
+      RA.traverse(TE.ApplicativeSeq)((userId) =>
         pipe(
           TE.tryCatch(
             () =>
@@ -65,38 +65,36 @@ export const runMassiveActivation = pipe(
                 trialId: config.TRIAL_ID,
                 body: {
                   userId,
-                  state: CreateSubscriptionStateEnum.ACTIVE
-                }
+                  state: CreateSubscriptionStateEnum.ACTIVE,
+                },
               }),
-            E.toError
+            E.toError,
           ),
           TE.chain(
             flow(
-              E.mapLeft(errs =>
-                Error(errorsToReadableMessages(errs).join("|"))
+              E.mapLeft((errs) =>
+                Error(errorsToReadableMessages(errs).join('|')),
               ),
               TE.fromEither,
               TE.filterOrElse(
-                res => res.status !== 500,
-                () => Error("Error while calling createSubscription API")
-              )
-            )
+                (res) => res.status !== 500,
+                () => Error('Error while calling createSubscription API'),
+              ),
+            ),
           ),
-          TE.map(res => {
+          TE.map((res) => {
             console.log(
-              `Activating user ${userId} on trial ${config.TRIAL_ID} => response ${res.status}`
+              `Activating user ${userId} on trial ${config.TRIAL_ID} => response ${res.status}`,
             );
             return res;
-          })
-        )
+          }),
+        ),
       ),
-      TE.map(() => "Success!")
-    )
+      TE.map(() => 'Success!'),
+    ),
   ),
   E.getOrElseW(() => TE.of(void 0)),
-  TE.toUnion
+  TE.toUnion,
 );
 
-runMassiveActivation()
-  .then(console.log)
-  .catch(console.error);
+runMassiveActivation().then(console.log).catch(console.error);
--- scripts/ops-scripts/src/utils/config.ts
+++ scripts/ops-scripts/src/utils/config.ts
@@ -1,16 +1,17 @@
-import { readableReportSimplified } from "@pagopa/ts-commons/lib/reporters";
-import { pipe } from "fp-ts/lib/function";
-import * as E from "fp-ts/Either";
-import * as t from "io-ts";
+import { readableReportSimplified } from '@pagopa/ts-commons/lib/reporters';
+import { pipe } from 'fp-ts/lib/function';
+import * as E from 'fp-ts/Either';
+import * as t from 'io-ts';
 
 export const getConfigOrThrow = <A, O>(
   configType: t.Type<A, O>,
-  environment: { [name: string]: string } | NodeJS.ProcessEnv
+  environment: Record<string, string> | NodeJS.ProcessEnv,
 ) =>
   pipe(
     environment,
     configType.decode,
-    E.getOrElseW(errs => {
+    E.getOrElseW((errs) => {
+      // eslint-disable-next-line functional/no-throw-statements
       throw new Error(readableReportSimplified(errs));
-    })
+    }),
   );
