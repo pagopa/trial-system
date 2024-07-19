5bad43b3e3b1da890ed77f09910aedd1f86d9428
AF <alessandro.ferlin@pagopa.it>
2024-07-19 12:21:50 +0200

    Move some file and refactor some other

--- scripts/k6-load-test/src/index.ts
+++ scripts/k6-load-test/src/index.ts
@@ -1,39 +1,35 @@
-import { getConfigOrThrow } from "./utils/config";
-import { pipe } from "fp-ts/lib/function";
-import * as E from "fp-ts/Either";
-import * as TE from "fp-ts/TaskEither";
-import * as AR from "fp-ts/Array";
-import { getFeatureScenario } from "./scenarios/mapping";
+import { parseConfigOrThrow } from './config';
+import { pipe } from 'fp-ts/lib/function';
+import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
+import * as AR from 'fp-ts/Array';
+import { getScenario } from './scenarios/get-scenario';
 
-const config = getConfigOrThrow(__ENV);
+const config = parseConfigOrThrow(__ENV);
 
 export const options = {
   scenarios: {
     contacts: {
-      executor: "ramping-arrival-rate",
+      executor: 'ramping-arrival-rate',
       startRate: 10,
-      // stages: [{target: 50, duration: "1m"}, {target: 100, duration: "1m"}, {target: config.rate, duration: "1m"}, {target: 400, duration: "3m"}],
-      stages: [
-        { target: config.rate, duration: "1m" },
-        { target: config.rate * 2, duration: "1m" }
-      ],
-      // How long the test lasts
-      // duration: config.duration,
+      stages: [{ target: config.rate, duration: config.duration }],
+      // Max VUs (concurrent users)
       maxVUs: config.maxVUs,
       // How many iterations per timeUnit
       // Start rate iterations per second
-      timeUnit: "1s",
+      timeUnit: '1s',
       // Pre-allocate VUs (concurrent users)
-      preAllocatedVUs: config.preAllocatedVUs
-    }
-  }
+      preAllocatedVUs: config.preAllocatedVUs,
+    },
+  },
 };
 
-export default async function() {
+export default async function () {
   await pipe(
-    config.SCENARIOS.map(getFeatureScenario),
-    scenarios => scenarios.map(fn => TE.tryCatch(() => fn(config), E.toError)),
-    AR.sequence(TE.ApplicativeSeq),
-    TE.toUnion
+    config.SCENARIOS.map(getScenario),
+    AR.traverse(TE.ApplicativeSeq)((scenario) =>
+      TE.tryCatch(() => scenario(config), E.toError),
+    ),
+    TE.toUnion,
   )();
 }
