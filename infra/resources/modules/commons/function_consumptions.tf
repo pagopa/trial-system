4f2be2064c2b65f528057ead4f16bb99898a7dcd
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-08-06 11:49:30 +0200

    [IOPLT-655] Turn on activation consumer (#166)

--- infra/resources/modules/commons/function_consumptions.tf
+++ infra/resources/modules/commons/function_consumptions.tf
@@ -35,7 +35,7 @@ locals {
     SUBSCRIPTION_REQUEST_EVENTHUB_NAME                             = local.subscription_request_eventhub_name
     SubscriptionRequestEventHubConnection__fullyQualifiedNamespace = "${module.event_hub.name}.servicebus.windows.net"
 
-    ACTIVATION_CONSUMER                                   = "off"
+    ACTIVATION_CONSUMER                                   = "on"
     ACTIVATION_MAX_FETCH_SIZE                             = "999"
     ACTIVATIONS_COSMOSDB_CONTAINER_NAME                   = azurerm_cosmosdb_sql_container.activation.name
     ActivationConsumerCosmosDBConnection__accountEndpoint = module.cosmosdb_account.endpoint
