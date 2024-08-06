a39dfc63b87ab656fd13e7b6d38d42ff6a7673cb
AF <4775679+datalek@users.noreply.github.com>
2024-08-06 09:59:05 +0200

    [IOPLT-652] Add new activation container (#161)

--- infra/resources/modules/commons/containers.tf
+++ infra/resources/modules/commons/containers.tf
@@ -42,6 +42,7 @@ resource "azurerm_cosmosdb_sql_container" "subscription_history" {
   }
 }
 
+# Remove this container once the application is migrated to container 'activation'
 resource "azurerm_cosmosdb_sql_container" "activations" {
   name                  = "activations"
   resource_group_name   = azurerm_resource_group.data_rg.name
@@ -72,6 +73,56 @@ resource "azurerm_cosmosdb_sql_container" "activations" {
 
       index {
         path  = "/activated"
+        order = "Ascending"
+      }
+
+      index {
+        path  = "/id"
+        order = "Ascending"
+      }
+    }
+  }
+}
+
+resource "azurerm_cosmosdb_sql_container" "activation" {
+  name                  = "activation"
+  resource_group_name   = azurerm_resource_group.data_rg.name
+  account_name          = module.cosmosdb_account.name
+  database_name         = module.cosmosdb_sql_database_trial.name
+  partition_key_path    = "/trialId"
+  partition_key_version = 2
+
+  # A unique key is scoped to a logical partition.
+  # Remember also that sparse unique keys are not supported. If some unique path
+  # values are missing, they're treated as null values, which take part in the
+  # uniqueness constraint. For this reason, there can be only a single item with
+  # a null value to satisfy this constraint.
+  unique_key {
+    paths = ["/userId"]
+  }
+
+  autoscale_settings {
+    max_throughput = 10000
+  }
+
+  indexing_policy {
+    included_path {
+      path = "/*"
+    }
+
+    composite_index {
+      index {
+        path  = "/trialId"
+        order = "Ascending"
+      }
+
+      index {
+        path  = "/type"
+        order = "Ascending"
+      }
+
+      index {
+        path  = "/state"
         order = "Ascending"
       }
 
