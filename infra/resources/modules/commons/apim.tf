92c0940f23be20bd57f8214f8e1b814d25e562ec
Marco Comi <9998393+kin0992@users.noreply.github.com>
2024-09-19 09:56:34 +0200

    Remove terraform data placeholder

--- infra/resources/modules/commons/apim.tf
+++ infra/resources/modules/commons/apim.tf
@@ -212,14 +212,6 @@ resource "azurerm_api_management_subscription" "test" {
 ####################################################################################
 # IO Backend
 ####################################################################################
-// TODO: Remove this when https://github.com/pagopa/trial-system/pull/192 is merged
-data "azurerm_api_management_user" "io_backend" {
-  api_management_name = module.apim.name
-  resource_group_name = module.apim.resource_group_name
-  user_id             = "io-backend-user-id"
-
-}
-    
 resource "azurerm_api_management_user" "io_backend" {
   user_id             = "io-backend"
   api_management_name = module.apim.name
@@ -236,12 +228,11 @@ resource "azurerm_api_management_group_user" "io_user_group" {
   resource_group_name = module.apim.resource_group_name
   group_name          = azurerm_api_management_group.api_trial_user.name
 }
-    
+
 resource "azurerm_api_management_subscription" "io_backend" {
   api_management_name = module.apim.name
   product_id          = module.apim_product_ts_management.id
-  // FIXME: Change user id when https://github.com/pagopa/trial-system/pull/192 is merged
-  user_id             = data.azurerm_api_management_user.io_backend.id
+  user_id             = azurerm_api_management_user.io_backend.id
   resource_group_name = module.apim.resource_group_name
   display_name        = "IO Backend"
   state               = "active"
