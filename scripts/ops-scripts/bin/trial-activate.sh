dc6307b09f250a52033cf862c2cd62eebe7ff523
AleDore <alessio85.dore@gmail.com>
2024-10-17 13:28:05 +0200

    script refactor for better usage experience

--- scripts/ops-scripts/bin/trial-activate.sh
+++ scripts/ops-scripts/bin/trial-activate.sh
@@ -1,8 +1,51 @@
 #!/bin/bash
-FILE_TO_SPLIT=$1
-CURRENTDATE=`date +"%Y-%m-%d"`
 
-read -p "How many lines(users) per chunk do you need to process? > " LINES_PER_CHUNK
+echo "Logging to Azure..."
+az login
+
+CURRENTDATE=`date +"%Y-%m-%d"`
+Color_Off='\033[0m'       # Text Reset
+Yellow='\033[0;33m'       # Yellow
+BYellow='\033[1;33m'      # Yellow
+Red='\033[0;31m'          # Red
+
+read -p "Do you want to load a local csv file (Y/N) [Default: Y]? > " LOAD_FROM_LOCAL_DRIVE
+LOAD_FROM_LOCAL_DRIVE=${LOAD_FROM_LOCAL_DRIVE:-Y}
+
+if [ "$LOAD_FROM_LOCAL_DRIVE" = "Y" ] ; 
+then
+    read -p "Please enter the local file path: > " FILE_TO_SPLIT
+else
+    echo -e "${Yellow}Remote csv files must be located into ${BYellow}iopstexportdata ${Yellow}storage account${Color_Off}"
+    FILE_EXISTS=false
+    while [ "$FILE_EXISTS" = false ];
+    do
+        read -p "Please enter a valid remote blob file path (including container i.e: input/trial-system/file.csv): > " REMOTE_FILE_PATH
+
+        REMOTE_CONTAINER_NAME="$(dirname "${REMOTE_FILE_PATH}")"
+        REMOTE_BLOB_NAME="$(basename "${REMOTE_FILE_PATH}")"
+        FILE_EXISTS=$(az storage blob exists --account-name iopstexportdata --container-name "$REMOTE_CONTAINER_NAME" --name "$REMOTE_BLOB_NAME" --query exists --output json)
+        
+        if [ "$FILE_EXISTS" = false ] ;
+        then
+            echo -e "${Red}====================================================${Color_Off}"
+            echo -e "${Red}The specified remote blob file path does not exists!${Color_Off}"
+            echo -e "${Red}====================================================${Color_Off}"
+        fi
+    done
+
+    echo "Downloading remote blob..."
+    FILE_TO_SPLIT="/tmp/$REMOTE_BLOB_NAME"
+
+    az storage blob download -f "$FILE_TO_SPLIT" --account-name iopstexportdata -c "$REMOTE_CONTAINER_NAME" -n "$REMOTE_BLOB_NAME" --no-progress
+fi
+NUMBER_OF_LINES=$(wc -l < $FILE_TO_SPLIT | xargs)
+((NUMBER_OF_LINES++))
+
+echo -e "${Yellow}Please note that you have selected a file that contains ${BYellow}$NUMBER_OF_LINES user/s${Color_Off}"
+
+read -p "How many users per chunk do you need to process [Default: $NUMBER_OF_LINES]? > " LINES_PER_CHUNK
+LINES_PER_CHUNK=${LINES_PER_CHUNK:-$NUMBER_OF_LINES}
 
 read -p "What's the Logic App url to be invoked? > " LAPP_URL
 
@@ -13,7 +56,7 @@ read -p "Please enter the api key to be used through the Trial System APIs: > " 
 read -p "Do you want to apply a delay between activations [Default: 0 ms]? > " DELAY
 DELAY=${DELAY:-0}
 
-read -p "Do you want to enable message notification flow for each activation [Default: false]? > " ENABLE_MESSAGE_FLOW
+read -p "Do you want to enable message notification flow for each activation (true/false) [Default: false]? > " ENABLE_MESSAGE_FLOW
 ENABLE_MESSAGE_FLOW=${ENABLE_MESSAGE_FLOW:-false}
 
 if [ "$ENABLE_MESSAGE_FLOW" = true ] ; 
@@ -23,9 +66,6 @@ then
     read -p "Please enter the api key used to send messages: > " MESSAGE_API_KEY
     read -p "Please enter the message template name to be used [listed on: https://github.com/pagopa/io-bulk-message/tree/main/templates]: > " MESSAGE_TEMPLATE_NAME
 fi
-
-echo "Logging to Azure..."
-az login
 
 ORIGINAL_FILENAME="$(basename "${FILE_TO_SPLIT}" ".csv")"
 ORIGINAL_PATH="$(dirname "${FILE_TO_SPLIT}")"
