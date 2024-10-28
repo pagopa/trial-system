4cd584b780895f8806cc905e5c88866c728cd6e8
robbcocco <robertococco@outlook.com>
2024-10-28 09:43:45 +0100

    [IOPLT-765] Add user Support PagoPA to ApiTrialSupport. (#249)

---
+++ .changeset/stupid-masks-yawn.md
@@ -0,0 +1,5 @@
+---
+"@infra/resources": minor
+---
+
+Add user Support PagoPA to `ApiTrialSupport` and create subscription key
--- infra/resources/modules/commons/apim.tf
+++ infra/resources/modules/commons/apim.tf
@@ -246,3 +246,33 @@ resource "azurerm_api_management_subscription" "io_backend" {
   state               = "active"
   allow_tracing       = false
 }
+
+####################################################################################
+# Support User
+####################################################################################
+resource "azurerm_api_management_user" "support" {
+  user_id             = "support-user"
+  api_management_name = module.apim.name
+  resource_group_name = module.apim.resource_group_name
+  first_name          = "Support"
+  last_name           = "PagoPA"
+  email               = "trialsystem-support@pagopa.it"
+  state               = "active"
+}
+
+resource "azurerm_api_management_group_user" "support" {
+  user_id             = azurerm_api_management_user.support.user_id
+  api_management_name = module.apim.name
+  resource_group_name = module.apim.resource_group_name
+  group_name          = azurerm_api_management_group.api_trial_support.name
+}
+
+resource "azurerm_api_management_subscription" "support" {
+  user_id             = azurerm_api_management_user.support.id
+  api_management_name = module.apim.name
+  resource_group_name = module.apim.resource_group_name
+  product_id          = module.apim_product_ts_management.id
+  display_name        = "PagoPA Support"
+  state               = "active"
+  allow_tracing       = false
+}
