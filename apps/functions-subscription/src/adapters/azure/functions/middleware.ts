86a15c8d58511559988862941a8004a0b046803e
PascalDR <akira.zeta@gmail.com>
2024-11-14 12:25:35 +0100

    Fix lint

--- apps/functions-subscription/src/adapters/azure/functions/middleware.ts
+++ apps/functions-subscription/src/adapters/azure/functions/middleware.ts
@@ -54,10 +54,8 @@ export const parseHeaderParameter =
 type AllowedGroup = 'ApiTrialUser' | 'ApiTrialManager' | 'ApiTrialSupport';
 
 const toTenantType = (groups: readonly string[]): Tenant['type'] => {
-  if(groups.some((group) => group === 'ApiTrialManager'))
-    return 'owner';
-  if(groups.some((group) => group === 'ApiTrialSupport'))
-    return 'support';
+  if (groups.some((group) => group === 'ApiTrialManager')) return 'owner';
+  if (groups.some((group) => group === 'ApiTrialSupport')) return 'support';
   return 'subscriber';
 };
 
